381d4b440e9c30a512a085d8a88b116c1332e69a
Leewansze <1226186467@qq.com>
2020-07-04 01:22:37 +0800

    ddl and dml practice

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
--- **Database level:**
+﻿-- **Database level:**
 -- Display all the database
+show databases;
 
 -- Enter a certain database
+use information_schema;
 
 -- Create a database
+create database_name;
 
 -- Create the database of the designated character set
+create database_name default CHARACTER set = 'utf8';
 
 -- Display the creation information fo the database
+show create database database_name;
 
 -- Revise the codes of the database
+alter database database_name charset gbk;
 
 -- Delete a database
+drop database database_name;
 
 -- **Table level**
 -- Revise table name
+alter table atable_name rename new_table_name;
 
 -- Revise the field's data type
+alter table atable_name modify field_name varchar;
 
 -- Revise field name
+alter table atable_name change old_field_name name new_field_name varchar [age];
 
 -- Add field
+alter table atable_name add field_information;
 
 -- Delete field
+alter table atable_name drop field_name;
 
 -- Revise the table's storage engine
+alter table atable_name engine=innodb;
 
 -- Delete the table's foreign key restriant
+alter table atable_name drop foreign key foreign_key_name;
 
 -- Delete a table
+drop table table_name;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
--- Insert record
+﻿-- Insert record
+insert into atable_name(field_name_1, field_name_2, field_name_3) values (data1, data2, data3);
 
 -- Revise record
+update atable_name set field_name_1 = new_data1, field_name2 = new_data2 [where];
 
 --  Delete record
+delete from atable_name [where];
 
 -- Search record
+select field_name_1, field_name_2 from atable_name [where];
